36c7de2e8662ad44968d8316101489e254bfbdd1
Markkusriis <xfj774@alumni.ku.dk>
2026-05-28 14:05:55 +0200

    Fixed merge issues

--- website/static/map.js
+++ website/static/map.js
@@ -4,7 +4,6 @@ const map = new maplibregl.Map({
     zoom: 11.3,
     container: 'map',
 });
-<<<<<<< Updated upstream
 
 let userLocationMarker = null;
 
@@ -14,17 +13,6 @@ const marker = new maplibregl.Marker()
         new maplibregl.Popup().setText("Caféen?")
     )
     .addTo(map);
-=======
-
-let userLocationMarker = null;
-
-// const marker = new maplibregl.Marker()
-//     .setLngLat([12.5585918, 55.7019809])
-//     .setPopup(
-//         new maplibregl.Popup().setText("Caféen?")
-//     )
-//     .addTo(map);
->>>>>>> Stashed changes
 
 async function loadVenues() {
     const response = await fetch("/auto_query");
@@ -60,23 +48,14 @@ function useMyLocation() {
                 userLocationMarker.remove();
             }
 
-<<<<<<< Updated upstream
-            userLocationMarker = new maplibregl.Marker()
+            userLocationMarker = new maplibregl.Marker({
+                color: '#F06516'
+            })
                 .setLngLat([lon, lat])
                 .setPopup(
                     new maplibregl.Popup().setText("You are here")
                 )
                 .addTo(map);
-=======
-            userLocationMarker = new maplibregl.Marker({
-                color: '#F06516'
-            })
-            .setLngLat([lon, lat])
-            .setPopup(
-                new maplibregl.Popup().setText("You are here")
-            )
-            .addTo(map);
->>>>>>> Stashed changes
         },
         (error) => {
             console.error(error);
