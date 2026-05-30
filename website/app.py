2738e962bd8d251ef37b79d165d03a7da05d9e6f
Markkusriis <xfj774@alumni.ku.dk>
2026-05-30 22:51:18 +0200

    Update app.py

--- website/app.py
+++ website/app.py
@@ -9,8 +9,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-conn = psycopg.connect(DATABASE_URL, sslmode="require")
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg.connect(DATABASE_URL, sslmode='require')
 
 
 app = Flask(__name__)
